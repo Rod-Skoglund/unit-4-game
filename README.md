4f0d911a98a398ff571c72fc4192c4e0e95850af
RLS <rod.skoglund@gmail.com>
2018-06-03 21:09:54 -0500

    Final working copy

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # unit-4-game
 Assignment #4 - Crystal Collector Game
+Author: Rod Skoglund
+Date: June 3, 2018
+
+#Table Of Contents
+----------------
+1. Game Introduction
+2. Instructions
+
+1. Game Introduction
+In this game, you will be given a target number and (4) gemstones (Blue, Red, Green and Yellow). The 4 gemstones will be assigned a unique, random number, but you do not know the value assigned to the gemstones. 
+
+Your starting score is 0. As you select the gemstones, the hidden value will be added to your score. You can select any combination of gemstones and you can select a gemstone multiple times.
+
+The goal of the game is to get your score to match the target number without going over. If your score is the same as the target number, you win. If your score is greater than teh target score, you lose. The game will keep track of the number of times you win and lose.
+
+2. Instructions
+The game starts when you first open or refresh the page. Either action will reset the number of wins and losses, establish a new target and reset your score to 0. This should only be done when you want to start the game from scratch. 
+
+Each round of the game will establish a new target value and different, unique values for the gemstones. These will be constant for that round of the game.
+
+To play the game, select gemstones to increase your score. Keep selecting gemstones until your score matches the target number or is greater than the target, at which point, the game will announce that you won or lost that round. 
+
+When you win or lose, a button will appear next to the gemstones that will allow you to start the next round. The button will be hidden during normal game play.
+
+Hint - As you play, try to determine the values assigned to the different gemstones so you can determine which combination will get you to the target.
+
+Enjoy ::):
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,38 +19,17 @@ var currentLossesCount = 0;
 var currentUserScore = 0;
 var gemImgArr = ["assets/images/blue-crystal.jpg", "assets/images/red-crystal.jpg", "assets/images/green-crystal.jpg", "assets/images/yellow-crystal.jpg"]
 
-
-// Define links to page elements
-// var currentTarget = document.getElementById("target");
-// var winCount = document.getElementById("wins");
-// var lossesCount = document.getElementById("losses");
-// var userScore = document.getElementById("score");
-
-function debugScript() {
-    // console.log("activeWord = " + activeWord);
-    // console.log("usedLetters = " + usedLetters);
-    // console.log("winCount = " + winCount);
-    // console.log("remainingGuesses = " + remainingGuesses);
-    // console.log("correctLetterCount = " + correctLetterCount);
-    // console.log("newGame = " + newGame);
-    // console.log("wordToDisplay = " + wordToDisplay);
-    // console.log("activeGuess = " + activeGuess);
-    // console.log("incorrectGuess = " + incorrectGuess);
-    // console.log("activeWord.length = " + activeWord.length);
-    // console.log("notAlreadyGuessed = " + notAlreadyGuessed);
-    console.log("*****************************");
-}
-
+//define script that must be run after page is loaded
 $(document).ready(function() {
 
-    //initialize target and gem values
+    //initialize target, gem values and reset user score to 0
     function initializeRandomValues() {
         randomTarget = Math.floor((Math.random() * 120) + 19);
         currentUserScore = 0;
 
         for (var i = 0; i < crystalsArr.length; i++) {
             var tempRandom = Math.floor((Math.random() * 12) + 1);
-            //Ensure the numbers are unique
+            //Ensure the random numbers for the 4 gems are unique
             if (crystalValueArray.indexOf(tempRandom) == -1) {
                 crystalValueArray[i] = tempRandom;
             }
@@ -58,29 +37,13 @@ $(document).ready(function() {
                 i--;
             }
         }
-        console.log("In initializeRandomValues");
-        console.log("randomTarget = " + randomTarget);
-        console.log("crystalValueArray[0] (blue) = " + crystalValueArray[0]);
-        console.log("crystalValueArray[1] (red) = " + crystalValueArray[1]);
-        console.log("crystalValueArray[2] (green) = " + crystalValueArray[2]);
-        console.log("crystalValueArray[3] (yellow) = " + crystalValueArray[3]);
-        console.log("**********************************************************");
-
     }    
-
-    
 
     //Get initial random values
     initializeRandomValues();
-    // console.log("randomTarget = " + randomTarget);
-    // console.log("crystalValueArray[0] = " + crystalValueArray[0]);
-    // console.log("crystalValueArray[1] = " + crystalValueArray[1]);
-    // console.log("crystalValueArray[2] = " + crystalValueArray[2]);
-    // console.log("crystalValueArray[3] = " + crystalValueArray[3]);
 
     //initialize Target Display
     var TargetDiv = $(".empty-div-target");
-    // var newTargetDiv = $("<div>" + randomTarget + "</div>");
     newTargetDiv = $("<div>");
     newTargetDiv.text(randomTarget);
     TargetDiv.append(newTargetDiv);
@@ -88,40 +51,36 @@ $(document).ready(function() {
     
     //initialize Win-Loss Display
     var WinLossDiv = $(".empty-win-lose");
+    
+    //Win Display with second display to allow easier update of the number of wins
     var newWinP = $("<p>");
     var newWinCountSpan = $("<span>");
-
     newWinP.text("Wins: ");
+
     newWinCountSpan.text(currentWinCount);
     newWinP.append(newWinCountSpan);
 
     WinLossDiv.append(newWinP);
     WinLossDiv.attr("class", "win-lose");
 
+    //Win Display with second display to allow easier update of the number of wins
     var newLossP = $("<p>");
     var newLossCountSpan = $("<span>");
-
     newLossP.text("Losses: ");
+
     newLossCountSpan.text(currentLossesCount);
     newLossP.append(newLossCountSpan);
 
-    // var newLossDiv = $("<div><p>Losses: " + currentLossesCount + "</p></div>");
     WinLossDiv.append(newLossP);
     WinLossDiv.attr("class", "win-lose");
 
     //initialize User Score Display
     var userScoreDiv = $(".empty-div-score");
-    // var newScoreDiv = $("<div>" + currentUserScore + "</div>");
     newScoreDiv =$("<div>");
     newScoreDiv.text(currentUserScore);
     userScoreDiv.append(newScoreDiv);
     userScoreDiv.attr("class", "score");
 
-    // gameStatus = $("<p>");
-    // gameStatus.addClass("gameStatusDisplay");
-    // gameStatus.text("select gem to start");
-    // userScoreDiv.append(gameStatus);
-    
     //initialize gemstones display
     for (var k = 0; k < crystalsArr.length; k++) {
         var gemImage = $("<img>");
@@ -132,127 +91,66 @@ $(document).ready(function() {
         $(".empty-gemstones").append(gemImage);
     }
 
+    //initialize restart button and add it to gemstone display - then hide it
+    restartBtn = $("<button>");
+    restartBtn.addClass("gemstones-img");
+    restartBtn.text("New Game");
+    restartBtn.hide();
+    $(".empty-gemstones").append(restartBtn);
+
+
     function updateDisplay() {
-        //update display
+        //update display values
         newTargetDiv.text(randomTarget);
         newWinCountSpan.text(currentWinCount);
         newLossCountSpan.text(currentLossesCount);
         newScoreDiv.text(currentUserScore);
-        // console.log("Update Display currentUserScore = " + currentUserScore);
-        // if (userState) {
-        //     alert("Congrats, You Win. Press OK to start a new game.");
-        // }
-        // else {
-        //     alert("Sorry, You lose. Press OK to start a new game.");
-        // }
     }
 
-    // function displayWinOrLoss (userWon) {
-    //     if (userWon) {
-    //         alert("Congrats, You Win. Press OK to start a new game.");
-    //     }
-    //     else {
-    //         alert("Sorry, You lose. Press OK to start a new game.");
-    //     }
-    // }
-
-    // function addSecs(d, s) {return new Date(d.valueOf()+s*1000);}
-    // function wasteTime(){
-    //     start = new Date();
-    //     end = addSecs(start,5);
-    //     do {start = new Date();} while (end-start > 0);
-    // }
-
     //Process gem selection event
-
-    //on gem click
     $(".gemstones-img").on("click", function() {
-        if (newGame) {
-            //Ignore reset gem selection and reset game
-            newGame = false;
-            // currentUserScore = 0;
-            initializeRandomValues();
-            updateDisplay();
-        }
-        else {
-            //process gem selection
+        //When the user wins or losses, ignore gem clicks until the new game button is selected
+        if (!newGame) {
             var GemColorSelected = $(this).attr("gemColor");
-            console.log("gem selectted = " + GemColorSelected);
-            // gameStatus.text("keep playing");
         
             //add specific gem value to user score
             currentUserScore += crystalValueArray[crystalsArr.indexOf(GemColorSelected)];
-            console.log("currentUserScore = " + currentUserScore);
 
             //update user score display
             newScoreDiv.text(currentUserScore);
-            var userWinOrLose = false;
-            var userWon;
 
             if (currentUserScore >= randomTarget) {
                 if (currentUserScore === randomTarget) {
                     currentWinCount++;
-                    // newScoreDiv.text(currentUserScore);
-                    // wasteTime();
-                    console.log("before alert currentUserScore = " + currentUserScore);
-                    // gameStatus.text("Congrats, You Win. Press OK to start a new game.");
-                    alert("Congrats, You Win. Select a Gem to start a new game.");
-                    // displayWinOrLoss(true);
-                    // updateDisplay();
-                    // userWinOrLose = true;
-                    // userWon = true;
+                    alert("Congrats, You Win. \nUse the New Game Button to start a new game.");
                     newGame = true;
+                    //show the restart button
+                    restartBtn.show();
                 }
                 else {
                     currentLossesCount++;
-                    console.log("before alert currentUserScore = " + currentUserScore);
-                    // newScoreDiv.text(currentUserScore);
-                    // gameStatus.text("Sorry, You lose. Press OK to start a new game.");
-                    // wasteTime();
-                    alert("Sorry, You lose. Select a Gem to start a new game.");               
-                    // displayWinOrLoss(false);
-                    // updateDisplay();
-                    // userWinOrLose = true;
-                    // userWon = false;
+                    alert("Sorry, You lose. \nUse the New Game Button to start a new game.");               
                     newGame = true;
+                    //show the restart button
+                    restartBtn.show();
                 }
                 
-                // initializeRandomValues();
-                // console.log("randomTarget = " + randomTarget);
-                // console.log("crystalValueArray[0] (blue) = " + crystalValueArray[0]);
-                // console.log("crystalValueArray[1] (red) = " + crystalValueArray[1]);
-                // console.log("crystalValueArray[2] (green) = " + crystalValueArray[2]);
-                // console.log("crystalValueArray[3] (yellow) = " + crystalValueArray[3]);
-                // console.log("**********************************************************");
-
-                // if (userWinOrLose) {
-                //     displayWinOrLoss(userWon)
-                // }
-
                 updateDisplay();
 
             }
         }
 
-    })
+    }) //End of Gem Click event processing
+
+    // Process Restart Button selection
+    restartBtn.on("click", function() {
+        newGame = false;
+        initializeRandomValues();
+        updateDisplay();
+        //Hide restart button
+        restartBtn.hide();
         
-        //if user score >= target
-            //if user score = target
-                //increment current win count
-                //update win count display
-                //alert user that they won and that a new game will start when they press OK
-            //else
-                //increment current losses count
-                //update losses count display
-                //alert user that they lost and that a new game will start when they press OK
-            //end if user score - target
-                //get new random values (target and all four gems)
-            //set user score to 0
-            //update disply to show new target and user score of 0
-        //end if - user score = target
-    // end gem click
+    }) //End of restart button click event processing
 
-    
-
-});
+}); //End of document ready section
     
